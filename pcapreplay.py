f19f65f2482fec41aef9a7cce136a6ca6e0b6a16
Denis Berezhnoy <denis.berezhnoy@gmail.com>
2024-12-24 18:38:50 -0500

    Code cleanup

--- pcapreplay.py
+++ pcapreplay.py
@@ -8,23 +8,31 @@ from enum import Enum
 VER_MAJOR = 0
 VER_MINOR = 4
 
+#
+# Physical link type
+#
 class LinkType(Enum):
     INVALID  = -1 # Invalid
     NULL     = 0  # BSD loopback encapsulation.
     ETHERNET = 1  # IEEE 802.3 Ethernet (10Mb, 100Mb, 1000Mb, and up)
-
+#
+# Protocol encapsulated in the payload of the eth frame
+#
 class EthType(Enum):
     INVALID  = -1 # Invalid
     IPv4     = 0  # IPv4
     IPv6     = 1  # IPv6
 
+#
+# Transport layer protocols
+#
 class TransportProtocol(Enum):
     INVALID  = -1 # Invalid
     TCP      = 0  # TCP
     UDP      = 1  # UDP
 
 #
-#
+# Ethernet frame with IPv4/TCP protocols
 #
 class Frame:
     def __init__(self):
