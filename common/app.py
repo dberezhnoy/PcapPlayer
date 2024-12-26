4f2c731b46068c5963ec7894c5d04cea8529f1e5
Denis Berezhnoy <denis.berezhnoy@gmail.com>
2024-12-25 19:08:28 -0500

    Major code restructure

---
+++ common/app.py
@@ -0,0 +1,37 @@
+VER_MAJOR = 0
+VER_MINOR = 4
+
+#
+# Ethernet frame with IPv4/TCP protocols
+#
+class Frame:
+    def __init__(self):
+        self.frame_num    = 0
+        self.captured_len = None
+        self.origin_len   = None
+        self.src_ip       = None
+        self.src_port     = None
+        self.dst_ip       = None
+        self.dst_port     = None
+        self.payload      = None
+        self.payload_len  = 0
+
+    def __eq__(self, other):
+        return self.frame_num == other.frame_num
+#
+# Application context
+#
+class Ctx:
+    def __init__(self):
+        # Input args
+        self.pcap_filename  = None
+        self.in_frame_nums  = None
+        self.replay_to_url  = None
+        self.delay_ms       = 0
+        # App context
+        self.frame_list    = None
+        self.cur_frame     = None
+        self.server_sock   = None
+
+URL_SCHEME_PLAIN_TCP = "plain-tcp"
+URL_SCHEME_TLS_TCP   = "tls-tcp"
---
+++ common/types.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+#
+# PCAP file type
+#
+class PcapType(Enum):
+    PCAP     = 0
+    PCAPNG   = 1
+
+#
+# Physical link type
+#
+class LinkType(Enum):
+    INVALID  = -1 # Invalid
+    NULL     = 0  # BSD loopback encapsulation.
+    ETHERNET = 1  # IEEE 802.3 Ethernet (10Mb, 100Mb, 1000Mb, and up)
+#
+# Protocol encapsulated in the payload of the eth frame
+#
+class EthType(Enum):
+    INVALID  = -1 # Invalid
+    IPv4     = 0  # IPv4
+    IPv6     = 1  # IPv6
+
+#
+# Transport layer protocols
+#
+class TransportProtocolType(Enum):
+    INVALID  = -1 # Invalid
+    TCP      = 0  # TCP
+    UDP      = 1  # UDP
